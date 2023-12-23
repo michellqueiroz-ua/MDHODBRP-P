41e3a83c0cdcbc4f026c5ae0a9d074013faff284
michellqueiroz <michellfelippe@dcc.ufba.br>
2023-12-23 15:46:49 +0100

    trying to solve infinite loop

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -11306,7 +11306,7 @@ int main(int argc, char **argv) {
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
 			//cout<<"HIER"<<endl;
 			input_travel_time(argv[i+1]);
-			/*for(int i=0;i<5828;i++) {
+			for(int i=0;i<5828;i++) {
 			for (int j=0;j<5828;j++) {
 				if (travel_time[i][j] == 0) {
 					cout<<i<<" "<<j<<endl;
@@ -11314,7 +11314,7 @@ int main(int argc, char **argv) {
 		  		}
 		  	}
 		  }
-		   cout <<endl;*/
+		   cout <<endl;
 		} else if (strcmp(argv[i], "--output_file") == 0) {
 			//cout<<"ttm"<<endl;
 			//input_travel_time(argv[i+1]);
