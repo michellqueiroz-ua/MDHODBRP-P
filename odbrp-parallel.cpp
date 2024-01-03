2a01e55fcd3b755a772e4588b7cfed6f9d71c42b
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-03 22:50:24 +0100

    aargh

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -12340,7 +12340,7 @@ int main(int argc, char **argv) {
 									//if (vehicle_assigned[nxt_p] == -1) {
 									//cout<<"hier5.75"<<endl;
 									//cout<<nxt_p<<" "<<px<<endl;
-									cout<<"it nxt cluster: "<<it_cl_inser[nxt_p].idx_cluster<<endl;
+									cout<<"it nxt cluster: "<<sort_clusters[nxt_p][it_cl_inser[nxt_p]].idx_cluster<<endl;
 									if (avl_cluster[px] == c) { //this way, each passenger has access only to one cluster at a time
 										if (sort_clusters[nxt_p][it_cl_inser[nxt_p]].idx_cluster == c) {
 											//cout<<"hier5.85"<<endl;
