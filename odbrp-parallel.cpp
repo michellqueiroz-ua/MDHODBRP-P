23c093f0894a6024ce59cc641c8ab1d1e174b20e
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-10 22:17:14 +0100

    try other stuff

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -6574,7 +6574,7 @@ void cheapest_insertion_randomized_parallel(int p, bool accept_infeasible_insert
 	//<<"almost exiting"<<endl;
 	for (int i=0;i<total_number_vehicles;i++)
 		blocked_vehicles[p][i] = 0;
-	//<<"exiting cluster: "<<cluster_id<<endl;
+	cout<<"exiting cluster: "<<cluster_id<<endl;
 }
 
 //this cheapest insertion considers to insert passengers at positions that are not the min increase in length traveled
@@ -12790,7 +12790,7 @@ int main(int argc, char **argv) {
 
 		//<<"xxxheeerexxxx1"<<endl;
 		//<<"actual passenger "<<k<<endl;
-		cout<<"hier7sa"<<endl;
+		//cout<<"hier7sa"<<endl;
 		if (k > total_number_vehicles + 10) {
 		
 			
@@ -12809,7 +12809,7 @@ int main(int argc, char **argv) {
 
 			}
 		}
-		cout<<"hier8sa"<<endl;
+		//cout<<"hier8sa"<<endl;
 
 		
 
@@ -12952,7 +12952,7 @@ int main(int argc, char **argv) {
 	std::ofstream output_file;
 	output_file.open(output_filename, std::ios::app);
 	output_file << requests_filename << " " << served_passengers << " " << served_passengers_3party << " " << total_served_passengers << " " << passengers_per_kilometer << " " << average_extra_travel_time << " " << average_travel_time_ratio << " " << total_deadheading_times << " " << total_shared_times << " " << total_user_ride_time << " " << best_total_user_ride_time;
-	cout<<" ";
+	output_file<<" ";
 	//previously already commented
 	/*cout << "served passengers ODB " << served_passengers << endl;
 	cout << "served passengers 3rd party: " << served_passengers_3party << endl;*/
