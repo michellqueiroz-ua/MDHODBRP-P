1f3a4b3be5a5554c8f38035ef43a5a4337c5975f
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-13 00:30:58 +0100

    jesus me ajuda

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -1279,7 +1279,7 @@ void input_requests_festival(char *filename) {
 		{
 
 			total_requests++;
-			if (total_requests > 19072) {
+			if (total_requests > 19031) {
 				total_requests--;
 				return;
 			}
