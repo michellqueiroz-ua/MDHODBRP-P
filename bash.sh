fb52517891bbfa9bf3f1b6f29ea59b375adebd3a
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-14 19:57:11 +0100

    commuting8.2

--- bash.sh
+++ bash.sh
@@ -1,18 +1,18 @@
 # Set the number of parallel processes
-num_parallel=1
+num_parallel=4
 
 # Define the function to execute your binary
 run_binary() {
 	input_file="$1"
 	seed1=502
     file_name=$(basename "$input_file")
-    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "festival1.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
+    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "commuting8.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
 }
 
 export -f run_binary
 
 # Use find to generate a list of input files and pass them to parallel
-find ../../large-instances/festival -type f -name "*.csv" | parallel -j $num_parallel run_binary {}
+find ../../large-instances/commuting -type f -name "*.csv" | parallel -j $num_parallel run_binary {}
 
 # Wait for all parallel processes to finish
 wait
--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define maxtotalcapacity 40
 #define maxtypevehicles 40
 #define maxnumberdepots 10
-#define number_clusters 1
+#define number_clusters 8
 
 typedef long unsigned listP[21000 + 1];
 //typedef int matrixVP[maxvehicles + 1][maxpassengers + 1];
@@ -12154,8 +12154,8 @@ int main(int argc, char **argv) {
 	for (int i=1; i<argc; i++)
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
-			input_requests_festival(argv[i+1]);
-			//input_requests_commuting(argv[i+1]);
+			//input_requests_festival(argv[i+1]);
+			input_requests_commuting(argv[i+1]);
 			requests_filename = argv[i+1];;
 			cout<<"x"<<total_requests<<" ";
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
@@ -12565,7 +12565,7 @@ int main(int argc, char **argv) {
 	p_updated_cluster = current_time;
 	//64800 - festival
 	//commuting - 32400
-	while((k < total_requests) or (current_time < 64800)) {
+	while((k < total_requests) or (current_time < 32400)) {
 	//while(current_time < 28800) {
 		
 		check_last_position_route();
