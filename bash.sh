80eac6bbcbce7f730210f08e8f7a9800a72a0b4f
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-14 20:01:13 +0100

    commuting5.2.2

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ run_binary() {
 	input_file="$1"
 	seed1=502
     file_name=$(basename "$input_file")
-    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "commuting6.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
+    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "commuting5.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
 }
 
 export -f run_binary
