27e6afef5922071b7afabf655a8c349a0a6f771a
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-19 19:37:38 +0100

    concert2prev1

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define maxtotalcapacity 40
 #define maxtypevehicles 40
 #define maxnumberdepots 10
-#define number_clusters 4
+#define number_clusters 1
 
 typedef int listP[21000 + 1];
 //typedef int matrixVP[maxvehicles + 1][maxpassengers + 1];
@@ -12319,8 +12319,8 @@ int main(int argc, char **argv) {
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
 			//input_requests_festival(argv[i+1]);
-			input_requests_commuting(argv[i+1]);
-			//input_requests(argv[i+1]); //concert & nightlife
+			//input_requests_commuting(argv[i+1]);
+			input_requests(argv[i+1]); //concert & nightlife
 			requests_filename = argv[i+1];;
 			cout<<"x"<<total_requests<<" ";
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
@@ -12738,7 +12738,8 @@ int main(int argc, char **argv) {
 	//commuting - 32400
 	//concert 42120(ta errado era pra ser 23.5, nao 11.5)
 	//nightlife - 88400 
-	while((k < total_requests) or (current_time < 32400)) {
+	//concert 2 - 70400
+	while((k < total_requests) or (current_time < 70400)) {
 	//while(current_time < 28800) {
 		
 		check_last_position_route();
@@ -12830,7 +12831,7 @@ int main(int argc, char **argv) {
 								//<<"cluster av: "<<avl_cluster[px]<<endl;
 								
 								//cout<<"bf_inser1 "<<avl_cluster[px]<<endl;
-								bool entered_here = false;
+								/*bool entered_here = false;
 								bool entered_there = false;
 								if (avl_cluster[px] == sort_clusters[nxt_p][0].idx_cluster){
 									entered_here = true;
@@ -12849,9 +12850,9 @@ int main(int argc, char **argv) {
 											}
 										}
 									}
-								}
+								}*/
 
-								//cheapest_insertion_randomized_parallel(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
+								cheapest_insertion_randomized_parallel(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
 								
 								//cout<<"af_inser1 "<<avl_cluster[px]<<endl;
 
