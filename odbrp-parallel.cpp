b74d8b2f879bd6d93fe767da5ad9a75231395667
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-21 11:37:02 +0100

    commuting1exp

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define maxtotalcapacity 40
 #define maxtypevehicles 40
 #define maxnumberdepots 10
-#define number_clusters 8
+#define number_clusters 1
 
 typedef int listP[21000 + 1];
 //typedef int matrixVP[maxvehicles + 1][maxpassengers + 1];
@@ -12319,8 +12319,8 @@ int main(int argc, char **argv) {
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
 			//input_requests_festival(argv[i+1]);
-			//input_requests_commuting(argv[i+1]);
-			input_requests(argv[i+1]); //concert & nightlife
+			input_requests_commuting(argv[i+1]);
+			//input_requests(argv[i+1]); //concert & nightlife
 			requests_filename = argv[i+1];;
 			cout<<"x"<<total_requests<<" ";
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
@@ -12738,7 +12738,7 @@ int main(int argc, char **argv) {
 	//commuting - 32400
 	//nightlife - 88400 
 	//concert 2 - 70400
-	while((k < total_requests) or (current_time < 70400)) {
+	while((k < total_requests) or (current_time < 32400)) {
 	//while(current_time < 28800) {
 		
 		check_last_position_route();
