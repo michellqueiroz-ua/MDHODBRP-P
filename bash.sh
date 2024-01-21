20ca8ed457be475a8fb94723381296fd0f287b64
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-21 11:51:04 +0100

    commuting1exp8

--- bash.sh
+++ bash.sh
@@ -8,7 +8,7 @@ run_binary() {
 		input_file="$1"
 		seed1=1504+iter
 	    file_name=$(basename "$input_file")
-	    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "commuting7.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
+	    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "commuting8.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
 	done
 }
 
