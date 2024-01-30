b7d5c0917f534e6569484f5aaf650fc48454ec4a
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-30 16:00:16 +0100

    atools teste

---
+++ mdhodbrp-atools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+module load atools
+
+run_file=parallelheur11.csv
+
+echo "input_file; seed1" > $run_file
+for input_file in `find ../../large-instances/festival -type f -name "*.csv"` ; do
+    counterx=0
+    for iter in 1 2 3 4 5
+    do
+        counterx=$((counterx+1))
+        seed1=0
+        seed1=$(($seed1 + $counterx))
+        file_name=$(basename "$input_file")
+        echo "$input_file; $seed1" >> $run_file
+    done
+done
+
+sbatch --array $(arange --data $run_file) mdhodbrp-atools.slurm $run_file
+
+
