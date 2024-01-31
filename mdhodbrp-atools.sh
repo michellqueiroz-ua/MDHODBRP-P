b853655fce3ca62fdf2afa98fc444908088f10fc
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 10:24:12 +0100

    commuting120k

--- mdhodbrp-atools.sh
+++ mdhodbrp-atools.sh
@@ -2,10 +2,10 @@
 
 module load atools
 
-run_file=parallelheur9.csv
+run_file=parallelheur10.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../large-instances/concert -type f -name "*.csv"` ; do
+for input_file in `find ../../large-instances/commuting20k -type f -name "*.csv"` ; do
     counterx=0
     for iter in 1 2 3 4 5
     do
