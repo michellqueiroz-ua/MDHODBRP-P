a5c44a75f924c326b538774cf18b79799440a6ac
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 17:34:31 +0100

    missingconodkkka2sss

--- mdhodbrp-atools.sh
+++ mdhodbrp-atools.sh
@@ -5,9 +5,9 @@ module load atools
 run_file=parallelheur18.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../large-instances/missingnight5 -type f -name "*.csv"` ; do
-    counterx=0
-    for iter in 1 4
+for input_file in `find ../../large-instances/missingconc2 -type f -name "*.csv"` ; do
+    counterx=6
+    for iter in 1
     do
         counterx=$((counterx+1))
         seed1=0
