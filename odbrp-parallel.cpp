424206fde94caff3af441e53366f77b5eb9e4c3b
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 14:54:32 +0100

    missing night22

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -12331,8 +12331,8 @@ int main(int argc, char **argv) {
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
 			//input_requests_festival(argv[i+1]);
-			input_requests_commuting(argv[i+1]);
-			//input_requests(argv[i+1]); //concert & nightlife
+			//input_requests_commuting(argv[i+1]);
+			input_requests(argv[i+1]); //concert & nightlife
 			requests_filename = argv[i+1];;
 			cout<<"x"<<total_requests<<" ";
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
