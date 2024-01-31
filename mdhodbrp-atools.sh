fc902d9588fdc56571a7f4de114def5f0be982b7
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 14:39:49 +0100

    run missing 1

--- mdhodbrp-atools.sh
+++ mdhodbrp-atools.sh
@@ -5,9 +5,9 @@ module load atools
 run_file=parallelheur10.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../large-instances/commuting20k -type f -name "*.csv"` ; do
+for input_file in `find ../../large-instances/misscomun -type f -name "*.csv"` ; do
     counterx=0
-    for iter in 1 2 3 4 5
+    for iter in 5
     do
         counterx=$((counterx+1))
         seed1=0
