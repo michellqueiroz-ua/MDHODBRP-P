32bffd7e0bc051c34abc6a61b1bf359465b66f0a
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 13:12:36 +0100

    festival120k2

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -12330,8 +12330,8 @@ int main(int argc, char **argv) {
 	for (int i=1; i<argc; i++)
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
-			//input_requests_festival(argv[i+1]);
-			input_requests_commuting(argv[i+1]);
+			input_requests_festival(argv[i+1]);
+			//input_requests_commuting(argv[i+1]);
 			//input_requests(argv[i+1]); //concert & nightlife
 			requests_filename = argv[i+1];;
 			cout<<"x"<<total_requests<<" ";
