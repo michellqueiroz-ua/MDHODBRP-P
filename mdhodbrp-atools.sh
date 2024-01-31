3e876259b58d017ed033f085a2911d8c24fe5747
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-31 15:51:40 +0100

    missingconc8

--- mdhodbrp-atools.sh
+++ mdhodbrp-atools.sh
@@ -5,9 +5,9 @@ module load atools
 run_file=parallelheur13.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../large-instances/missingconc -type f -name "*.csv"` ; do
+for input_file in `find ../../large-instances/missingconc2 -type f -name "*.csv"` ; do
     counterx=0
-    for iter in 4
+    for iter in 1 4
     do
         counterx=$((counterx+1))
         seed1=0
