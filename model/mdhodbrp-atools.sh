9fa6cfd14e8197ce5f8400465dd5b6c54c96c17c
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-03-21 23:49:17 +0100

    adding gurobi model

---
+++ model/mdhodbrp-atools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+module load atools
+mudole load Gurobi
+
+run_file=parallelheur18.csv
+
+echo "input_file; seed1" > $run_file
+for input_file in `find ../../large-instances/missingconc2 -type f -name "*.csv"` ; do
+    counterx=6
+    for iter in 1
+    do
+        counterx=$((counterx+1))
+        seed1=0
+        seed1=$(($seed1 + $counterx))
+        file_name=$(basename "$input_file")
+        echo "$input_file; $seed1" >> $run_file
+    done
+done
+
+sbatch --array $(arange --data $run_file) mdhodbrp-atools.slurm $run_file
+
+
