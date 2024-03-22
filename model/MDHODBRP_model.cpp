9f03b9d9afc219ce4d119424a017e1421b68d667
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-03-22 09:51:37 +0100

    see if compiles

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -85,7 +85,7 @@ void MDHODBRPFR_MODEL(){
 		model.setObjective(objFunc, GRB_MINIMIZE);
 
 		//(2)
-		for (int r = 0; r < number_requests; r++){
+		/*for (int r = 0; r < number_requests; r++){
 
 			GRBLinExpr sum = 0;
 			for (int b = 0; b < total_number_vehicles; b++) {
@@ -363,7 +363,7 @@ void MDHODBRPFR_MODEL(){
 				//sum.end();
 
 			}
-		}
+		}*/
 
 
 		//---
