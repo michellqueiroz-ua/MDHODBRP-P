8711b3a53073ba87b5ffd840e7e145db8323c206
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-04-09 16:01:41 +0200

    try 3 req

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -1184,6 +1184,10 @@ int main(int argc, char **argv) {
 		number_stops_destination[i] = 2;
 	}
 
+	for (int k=0;k<total_requests;k++){
+		latest_arrival[k] = latest_arrival[k] + 1800;
+	}
+
 	cout<<"success"<<endl;
 	cout<<"HIER "<<total_number_vehicles<<" "<<number_stops_origin[0]<<" "<<number_nodes<<endl;
 	MDHODBRPFR_MODEL();
