58ad7864b2467029397396d942b84744e6f1a6d2
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-05-29 16:30:35 +0200

    debugging model3

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -947,7 +947,7 @@ void MDHODBRPFR_MODEL(){
 
 		//(12)
 		//question here -> if the node isn't served. what T[b][i1] will be? 
-		/*for (int b = 0; b < total_number_vehicles; b++) {
+		for (int b = 0; b < total_number_vehicles; b++) {
 			for (int r = 0; r < total_requests; r++){
 				for (int i = 0; i < number_stops_origin[r]; i++) {
 					int i1 = stops_origin[r][i];
@@ -959,11 +959,11 @@ void MDHODBRPFR_MODEL(){
 					//sum.end();	
 				}
 			}
-		}*/
+		}
 
 		//*new constraints*
 		// try to force T[b][i1] to zero if node isn't served
-		for (int b = 0; b < total_number_vehicles; b++) {
+		/*for (int b = 0; b < total_number_vehicles; b++) {
 			for (int r = 0; r < total_requests; r++){
 				for (int i = 0; i < number_nodes; i++) {
 					//int i1 = stops_origin[r][i];
@@ -1000,7 +1000,7 @@ void MDHODBRPFR_MODEL(){
 			model.addConstr(sum - sum2 >= 0);
 
 
-		}
+		}*/
 
 		//ensure that a passenger destination is served before its origin
 
