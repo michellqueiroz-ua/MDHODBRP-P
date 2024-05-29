a585ade6f7bf1e9a0e1bab2d9a3a433d3e4c1e8d
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-05-29 16:07:26 +0200

    testing infeasibility mode4

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -731,7 +731,7 @@ void MDHODBRPFR_MODEL(){
 			//sum.end();
 
 		}*/
-			
+
 
 		//(7)
 		//picked up and dropped by the same vehicle
@@ -1067,7 +1067,7 @@ void MDHODBRPFR_MODEL(){
 
 
 		// try to force T[b][i1] to zero if node isn't served
-		/*for (int b = 0; b < total_number_vehicles; b++) {
+		for (int b = 0; b < total_number_vehicles; b++) {
 			for (int r = 0; r < total_requests; r++){
 				for (int i = 0; i < number_nodes; i++) {
 					//int i1 = stops_origin[r][i];
@@ -1079,7 +1079,7 @@ void MDHODBRPFR_MODEL(){
 					//sum.end();	
 				}
 			}
-		}*/
+		}
 
 		//(13)
 		for (int b = 0; b < total_number_vehicles; b++) {
