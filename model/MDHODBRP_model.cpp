d8e6c07ecc10c0e321013de3a7a95fce489b142d
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-05-29 16:05:16 +0200

    testing infeasibility mode2

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -714,10 +714,9 @@ void MDHODBRPFR_MODEL(){
 		//(6)
 		//every request is served once
 
-		for (int r = 0; r < total_requests; r++){
+		/*for (int r = 0; r < total_requests; r++){
 
 			GRBLinExpr sum = 0;
-
 			for (int b = 0; b < total_number_vehicles; b++) {
 				for (int i = 0; i < number_stops_origin[r]; i++) {
 					int nodei = stops_origin[r][i];
@@ -731,7 +730,7 @@ void MDHODBRPFR_MODEL(){
 			//model.add(sum == 1);
 			//sum.end();
 
-		}
+		}*/
 
 		//(7)
 		//picked up and dropped by the same vehicle
@@ -964,7 +963,7 @@ void MDHODBRPFR_MODEL(){
 
 		//*new constraints*
 		// try to force T[b][i1] to zero if node isn't served
-		/*for (int b = 0; b < total_number_vehicles; b++) {
+		for (int b = 0; b < total_number_vehicles; b++) {
 			for (int r = 0; r < total_requests; r++){
 				for (int i = 0; i < number_nodes; i++) {
 					//int i1 = stops_origin[r][i];
@@ -976,7 +975,7 @@ void MDHODBRPFR_MODEL(){
 					//sum.end();	
 				}
 			}
-		}*/
+		}
 
 		for (int r = 0; r < total_requests; r++){
 
