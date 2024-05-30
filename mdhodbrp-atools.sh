3086d84926cb81d7dea3af3f13f85e85333991bd
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-05-30 13:55:35 +0200

    testing rejecting rate2

--- mdhodbrp-atools.sh
+++ mdhodbrp-atools.sh
@@ -5,7 +5,7 @@ module load atools
 run_file=parallelheur.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../instances/300_180_1000 -type f -name "*.csv"` ; do
+for input_file in `find ../../instances/uneven_demand -type f -name "*.csv"` ; do
     counterx=6
     for iter in 1
     do
