9942708412a2a512320187c2ba3f176d9c9681b9
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-07 23:22:12 +0100

    t25again

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -1612,8 +1612,8 @@ int main(int argc, char **argv) {
 
    	int k = 0;
    	number_nodes_depots = 0;
-   	total_requests = 20;
-	total_number_vehicles = 7;
+   	total_requests = 25;
+	total_number_vehicles = 8;
 	number_vehicles[0] = 8;
 	number_vehicles[1] = 0;
 	number_vehicles[2] = 0;
