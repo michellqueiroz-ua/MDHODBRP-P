f73c33a1cda124e711e4d2745aa69dab404be2ae
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-10 22:57:19 +0100

    t 15 with 6 v7

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -14999,7 +14999,7 @@ int main(int argc, char **argv) {
 
 	//ensure feasibility
 	for (int k=0;k<total_requests;k++){
-		latest_arrival[k] = latest_arrival[k] + 1800;
+		latest_arrival[k] = latest_arrival[k] + 2400;
 	}
 
 	//while((k < total_requests) or (current_time < 32400)) {
