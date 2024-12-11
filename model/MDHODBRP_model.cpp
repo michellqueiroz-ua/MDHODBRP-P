6144a808dc116c8fad0871135cafea60be6bbe4f
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-11 20:47:47 +0100

    model 10 3

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -1613,8 +1613,8 @@ int main(int argc, char **argv) {
    	int k = 0;
    	number_nodes_depots = 0;
    	total_requests = 15;
-	total_number_vehicles = 8;
-	number_vehicles[0] = 8;
+	total_number_vehicles = 6;
+	number_vehicles[0] = 6;
 	number_vehicles[1] = 0;
 	number_vehicles[2] = 0;
 	/*for (int i=0; i<total_requests; i++){
