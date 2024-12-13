dd2d38d0e3305845ffd1200c130537f7aaf3bdc9
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-14 00:54:27 +0100

    5 2

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -14673,8 +14673,8 @@ int main(int argc, char **argv) {
 	//<<"staart "<<number_type_vehicles<<" "<<endl;
 	
 	//remove this
-	total_requests = 7;
-	total_number_vehicles = 4;
+	total_requests = 5;
+	total_number_vehicles = 2;
 	for (int i=0; i<total_requests; i++){
 		if (number_stops_origin[i] > 3)
 			number_stops_origin[i] = 3;
