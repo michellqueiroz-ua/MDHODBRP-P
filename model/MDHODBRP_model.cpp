59a9f6912e5e13769804cd3b17a0f712f7dc09cc
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-18 20:53:55 +0100

    16 model

--- model/MDHODBRP_model.cpp
+++ model/MDHODBRP_model.cpp
@@ -160,7 +160,7 @@ void input_requests(char *filename) {
 	int p, s;
 
 
-	int max_number_requests_read = 15;
+	int max_number_requests_read = 16;
 	number_nodes = 0;
 	if(file.is_open())
 	{
@@ -1612,8 +1612,8 @@ int main(int argc, char **argv) {
 
    	int k = 0;
    	number_nodes_depots = 0;
-   	total_requests = 15;
-	total_number_vehicles = 6;
+   	total_requests = 16;
+	total_number_vehicles = 7;
 	number_vehicles[0] = 6;
 	number_vehicles[1] = 0;
 	number_vehicles[2] = 0;
