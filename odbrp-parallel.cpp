39992128df061124363c9906c500a580d34b8c7d
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-12-20 21:21:02 +0100

    testing if S13

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -14683,8 +14683,8 @@ int main(int argc, char **argv) {
 	//<<"staart "<<number_type_vehicles<<" "<<endl;
 	
 	//remove this
-	total_requests = 12;
-	total_number_vehicles = 5;
+	total_requests = 13;
+	total_number_vehicles = 6;
 	for (int i=0; i<total_requests; i++){
 		if (number_stops_origin[i] > 3)
 			number_stops_origin[i] = 3;
