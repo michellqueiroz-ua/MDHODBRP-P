91d00615d9664848791a9abb2b76a97f0e56a2d5
michellqueiroz <michellfelippe@dcc.ufba.br>
2025-03-08 12:04:05 +0100

    starting experiments to report swaaap

--- mdhodbrp-atools4.sh
+++ mdhodbrp-atools4.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 module load atools
+module load calcua/2024a GCCcore/13.3.0
 
 run_file=parallelheur4.csv
 
 echo "input_file; seed1" > $run_file
-for input_file in `find ../../instances/1200_ni -type f -name "*.csv"` ; do
+for input_file in `find ../../instances/EXP_NN/1200_NN -type f -name "*.csv"` ; do
     counterx=6
-    for iter in 1
+    for iter in {1..10}
     do
         counterx=$((counterx+1))
-        seed1=0
+        seed1=111
         seed1=$(($seed1 + $counterx))
         file_name=$(basename "$input_file")
         echo "$input_file; $seed1" >> $run_file
     done
 done
 
-sbatch --account ap_enm --array $(arange --data $run_file) mdhodbrp-atools4.slurm $run_file
+sbatch --account ap_enm --array $(arange --data $run_file) mdhodbrp-atools.slurm $run_file
 
 
