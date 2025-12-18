bdee4bc0683f14b30d929197784b4698e71c7806
Michell Queiroz <micqu@dtu.dk>
2025-12-18 21:55:31 +0100

    trying again5

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -16542,28 +16542,28 @@ int main(int argc, char **argv) {
 								cout<<"cluster av: "<<avl_cluster[px]<<endl;
 								
 								//cout<<"bf_inser1 "<<avl_cluster[px]<<endl;
-								/*bool entered_here = false;
+								bool entered_here = false;
 								bool entered_there = false;
 								if (avl_cluster[px] == sort_clusters[nxt_p][0].idx_cluster){
 									entered_here = true;
-									cheapest_insertion_randomized_parallel(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
+									cheapest_insertion_randomized_parallel_NEW(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
 								} else {
 									if (number_clusters > 1) {
 										if (avl_cluster[px] == sort_clusters[nxt_p][1].idx_cluster){
 											entered_here = true;
-											cheapest_insertion_randomized_parallel(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
+											cheapest_insertion_randomized_parallel_NEW(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
 										} else {
 											double y = (double)rand() / (double)RAND_MAX;
 											if (y <= 0.5) {
 												entered_there = true;
 												//entered_here = true;
-												cheapest_insertion_randomized_parallel(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
+												cheapest_insertion_randomized_parallel_NEW(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
 											}
 										}
 									}
-								}*/
+								}
 
-								cheapest_insertion_randomized_parallel_NEW(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
+								//cheapest_insertion_randomized_parallel_NEW(nxt_p, accept_infeasible_insertion, avl_cluster[px]);
 								
 								cout<<"af_inser1 "<<avl_cluster[px]<<endl;
 
