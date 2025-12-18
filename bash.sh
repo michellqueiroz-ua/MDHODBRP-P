65367cdd382ba11968ab01da4997e712bc0120e8
Michell Queiroz <micqu@dtu.dk>
2025-12-18 16:16:52 +0100

    trying last thing to see if it works

--- bash.sh
+++ bash.sh
@@ -1,17 +1,17 @@
 # Set the number of parallel processes
-num_parallel=4
+num_parallel=2
 
 # Define the function to execute your binary
 run_binary() {
 	counterx=0
-	for iter in 1 2 3 4 5
+	for iter in 1 2 3 4 5 6 7 8 9 10
 	do
 		counterx=$((counterx+1))
 		input_file="$1"
 		seed1=0
 		seed1=$(($seed1 + $counterx))
 	    file_name=$(basename "$input_file")
-	    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "AfestivalS16.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 7 --number_vehicles2 7 --number_vehicles3 7 --capacity_vehicles 8 16 32 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
+	    ./a.out --filename_requests $input_file --seed $seed1 --filename_travel_time "travel_time_updated3.csv" --output_file "R1_festival_S2.txt" --number_depots 3 --depot 5825 5826 5827 --type_vehicles 3 --number_vehicles1 15 --number_vehicles2 8 --number_vehicles3 0 --capacity_vehicles 4 8 12 --init_temperature 1.3 --lamba 0.9 --maxnrep 350 --increase_rep 800 --total_requests 900  
 	done
 }
 
