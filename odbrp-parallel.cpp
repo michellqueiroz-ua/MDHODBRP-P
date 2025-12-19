e5995cc41783b96a4dee204cfbf1e38843510858
Michell Queiroz <micqu@dtu.dk>
2025-12-19 23:03:31 +0100

    removing comments and testing 4 cores38

--- odbrp-parallel.cpp
+++ odbrp-parallel.cpp
@@ -15964,9 +15964,9 @@ int main(int argc, char **argv) {
 	for (int i=1; i<argc; i++)
   	{
 		if (strcmp(argv[i], "--filename_requests") == 0) {
-			//input_requests_festival(argv[i+1]);
+			input_requests_festival(argv[i+1]);
 			//input_requests_commuting(argv[i+1]);
-			input_requests(argv[i+1]); //concert & nightlife
+			//input_requests(argv[i+1]); //concert & nightlife
 			requests_filename = argv[i+1];
 			cout<<"x"<<total_requests<<" ";
 		} else if (strcmp(argv[i], "--filename_travel_time") == 0) {
